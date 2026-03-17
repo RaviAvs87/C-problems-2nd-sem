09a33c05e6c21600c20eccc43118d2c572f8c7a8
Anshuman Verma <kalkishishya@gmail.com>
2026-03-17 20:38:08 +0530

    Create 06 printing pattern.c

---
+++ 06 printing pattern.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int i,j;
+    for(i = 97; i<=101; i++){
+        for(j = 97; j<=i; j++){
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+}
